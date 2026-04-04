a3f1164b7d748716f8aa771a49d2b0022bfe70a0
Arshia Aggarwal <arshiaaggarwal295@gmail.com>
2026-04-04 21:30:46 +0530

    Made 3 changes to q2 code fixing t0 mainly

--- q2/q2.s
+++ q2/q2.s
@@ -1,8 +1,9 @@
 .globl main
-
 .section .rodata
+
 fmt: .string "%lld "
 
+newline: .string "\n"
 .section .text
 
 main:
@@ -19,18 +20,15 @@ main:
     mv s1, a1          # s1 = argv
     addi s0, s0, -1    # s0 = n = argc - 1
 
-
     # malloc arr
     slli a0, s0, 3
     call malloc
     mv s2, a0          # s2 = arr[]
-
-
+    
     # malloc result
     slli a0, s0, 3
     call malloc
     mv s3, a0          # s3 = result[]
-
 
     # malloc stack 
     slli a0, s0, 3
@@ -38,32 +36,32 @@ main:
     mv s4, a0          # s4 = stack[]
 
     li s5, -1          # s5 = top = -1
-
     li t0, 0           # loop counter
 
 input_loop:
-
     bge t0, s0, read_done   # if loop counter = n-1 then go to read_done
     addi t1, t0, 1          # here t1 = t0 + 1 (we do this because we are skipping /a.out)
 
-    slli t1, t1, 3      # to calculate offset
+    slli t1, t1, 3          # to calculate offset
     add t1, s1, t1          # s1 acts as a pointer to argv so t1+s1 acts as pointer to that element
-    ld a0, 0(t1)       # a0 = argv[i+1]
+    ld a0, 0(t1)            # a0 = argv[i+1]
 
-    mv a1, t0          # save loop counter in a1 before call (or use stack)
-    call atoi          # converts a0 to int value as it reads it as string
-    mv t0, a1          # restore loop counter after call
+    addi sp, sp, -8         # save t0 around atoi call (atoi clobbers t registers)
+    sd t0, 0(sp)
+    call atoi               # converts a0 to int value as it reads it as string
+    ld t0, 0(sp)            # restore t0
 
-    slli t1, t0, 3     # we again restart the process 
-    add t1, s2, t1     # this is for storing values in array
-    sd a0, 0(t1)       
+    addi sp, sp, 8
+    slli t1, t0, 3          # we again restart the process 
+    add t1, s2, t1          # this is for storing values in array
+    sd a0, 0(t1)   
 
-    slli t1, t0, 3      # yet again we do this this time for result (here we make all result as -1)
+    slli t1, t0, 3          # yet again we do this this time for result (here we make all result as -1)
     add t1, s3, t1      
     li t2, -1
     sd t2, 0(t1)
 
-    addi t0, t0, 1     # increment counter
+    addi t0, t0, 1          # increment counter
     j input_loop
 
 read_done:
@@ -71,60 +69,64 @@ read_done:
 
 nge:
     blt t0, x0, nge_done    # we start from behind and the second it is less than 0 we got to nge_done
+    
     slli t1, t0, 3
     add t1, s2, t1
     ld t3, 0(t1)
 
 while:
+    blt s5, x0, push        # initially s5 is -1 and when stack is empty it goes to push
 
-    blt s5, x0, push   # initally s5 is -1 and when stack is empty it goes to push
-    
-    
     slli t1, s5, 3              # t1 = s5 * 8
     add t1, s4, t1              # t1 = t1 + s4 (t1 is the offset to reach top of stack)
-    ld t4, 0(t1)                 # t4 = index of value in array at top of stack
+    ld t4, 0(t1)                # t4 = index of value in array at top of stack
 
-    slli t1, t4, 3             # here we are loading the value of that index form array into t5
+    slli t1, t4, 3              # here we are loading the value of that index from array into t5
     add t1, s2, t1
     ld t5, 0(t1)               
 
-    bgt t5, t3, after_while        # arr[stack[top]] > arr[i] we continue to while-done
-    addi s5, s5, -1               # s5 is basically maintaining count of the number of elements-1 in stack
-    j while
-    
-after_while:
-    slli t1, t0, 3               # t1 = offest which is i*8
-    add t1, s3, t1                  # we add this offset to the result array (t1 is now the address where we store the result)
-    sd t4, 0(t1)      
+    bgt t5, t3, after_while     # arr[stack[top]] > arr[i] we continue to after-while
 
+    addi s5, s5, -1             # s5 is basically maintaining count of the number of elements-1 in stack
+    j while
+
+after_while:
+    slli t1, t0, 3              # t1 = offset which is i*8
+    add t1, s3, t1              # we add this offset to the result array (t1 is now the address where we store the result)
+    sd t4, 0(t1)      
     j push
 
 push:
-
-    addi s5, s5, 1                  # increase size of stack
-    
+    addi s5, s5, 1              # increase size of stack
     slli t1, s5, 3
-    add t1, s4, t1                 # this is the top of stack memory location
-    sd t0, 0(t1)                   #  we store t0 which is i in there
-
-    addi t0, t0, -1                 # decrease counter
+    add t1, s4, t1              # this is the top of stack memory location
+    sd t0, 0(t1)                # we store t0 which is i in there
+    addi t0, t0, -1             # decrease counter
     j nge
 
 nge_done:
     mv t0, x0
 
 print:
-    bge t0, s0, done
+    bge t0, s0, print_done
 
     slli t1, t0, 3
-    add t1, s3, t1  # result array
+    add t1, s3, t1              # result array
+    ld a1, 0(t1)         
 
-    ld a1, 0(t1)              
-    la a0, fmt                
+    la a0, fmt
+    addi sp, sp, -8             # save t0 around printf call (printf clobbers t registers)
+    sd t0, 0(sp)
     call printf
 
-    addi t0, t0, 1   # increment the loop
+    ld t0, 0(sp)                # restore t0
+    addi sp, sp, 8
+    addi t0, t0, 1              # increment the loop
     j print
+
+print_done:
+    la a0, newline              # flush output with newline
+    call printf
 
 done:
     ld ra, 56(sp)
@@ -136,5 +138,3 @@ done:
     ld s5, 8(sp)
     addi sp, sp, 64
     ret
-
-    
