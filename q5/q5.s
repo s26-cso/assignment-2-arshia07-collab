df3c5ff97ec2d2866fe4482ffcc6881adc6101ec
Arshia Aggarwal <arshiaaggarwal295@gmail.com>
2026-04-05 00:55:17 +0530

    update

--- q5/q5.s
+++ q5/q5.s
@@ -1,8 +1,8 @@
 .section .rodata
 
 filename: .string "input.txt"
-yes_String:  .string "Yes"
-no_String:   .string "No"
+yes_String:  .string "Yes\n"
+no_String:   .string "No\n"
 
 .section .text
 
@@ -22,10 +22,12 @@ main:
     sd s1, 8(sp)
     sd s2, 0(sp)
 
-    # open("input.txt", O_RDONLY)
-    la a0, filename       # a0 = filename
-    li a1, 0              # a1 = O_RDONLY
-    li a7, 1024           # syscall to open
+    # openat(AT_FDCWD, "input.txt", O_RDONLY)
+    li a0, -100           # AT_FDCWD
+    la a1, filename       # a1 = filename
+    li a2, 0              # a2 = O_RDONLY
+    li a3, 0
+    li a7, 56             # syscall: openat
     ecall
     mv s0, a0             # s0 = fd
 
@@ -33,12 +35,28 @@ main:
     mv a0, s0
     li a1, 0
     li a2, 2              # SEEK_END
-    li a7, 1025           # syscall to lseek
+    li a7, 62             # syscall: lseek
     ecall
 
     mv s2, a0             # s2 = size
     addi s2, s2, -1       # right = size - 1 (last byte index)
     mv s1, x0              # left = 0
+
+    # peek at last byte to check for trailing newline
+    mv a0, s0
+    mv a1, s2
+    li a2, 0              # SEEK_SET
+    li a7, 62             # syscall: lseek
+    ecall
+    mv a0, s0
+    addi a1, sp, 28       # temp buffer (inside stack frame)
+    li a2, 1
+    li a7, 63             # syscall: read
+    ecall
+    lb t0, 28(sp)
+    li t1, 10             # '\n' = ASCII 10
+    bne t0, t1, loop      # no trailing newline, right is already correct
+    addi s2, s2, -1       # trailing newline found, skip it
 
 
 loop:
@@ -51,7 +69,7 @@ loop:
     mv a0, s0
     mv a1, s1
     li a2, 0              # SEEK_SET
-    li a7, 1025
+    li a7, 62             # syscall: lseek
     ecall
 
 
@@ -70,7 +88,7 @@ loop:
     mv a0, s0
     mv a1, s2
     li a2, 0
-    li a7, 1025
+    li a7, 62             # syscall: lseek
     ecall
 
     # read(fd, &buf, 1)
@@ -82,25 +100,7 @@ loop:
 
     lb t1, 29(sp)         # t1 = right char
 
-    # if right char is newline, skip it and re-read
-    li t2, 10             # '\n' = ASCII 10
-    bne t1, t2, compare
-    addi s2, s2, -1       # right-- to skip newline
-    # lseek(fd, right, SEEK_SET)
-    mv a0, s0
-    mv a1, s2
-    li a2, 0
-    li a7, 1025
-    ecall
-    # read(fd, &buf, 1)
-    mv a0, s0
-    addi a1, sp, 29       # temp buffer (inside stack frame)
-    li a2, 1
-    li a7, 63
-    ecall
-    lb t1, 29(sp)         # t1 = right char (after skipping newline)
 
-compare:
     bne t0, t1, no
 
 
@@ -110,15 +110,19 @@ compare:
 
 
 yes:
-    la a0, yes_String
-    li a7, 4              # syscall: print string
+    li a0, 1              # fd = stdout
+    la a1, yes_String
+    li a2, 4              # length of "Yes\n"
+    li a7, 64             # syscall: write
     ecall
     j done
 
 
 no:
-    la a0, no_String
-    li a7, 4              # syscall: print string
+    li a0, 1              # fd = stdout
+    la a1, no_String
+    li a2, 3              # length of "No\n"
+    li a7, 64             # syscall: write
     ecall
 
 done:
@@ -127,4 +131,7 @@ done:
     ld s1, 8(sp)
     ld s2, 0(sp)
     addi sp, sp, 32
-    ret
+    li a0, 0              # exit code 0
+    li a7, 93             # syscall: exit
+    ecall
+    
